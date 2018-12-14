f1ec317dca38d44832098e8a96b8ea3420c89195
Rob THomas <s4developer@gmail.com>
2018-12-13 19:06:43 -0600

    Update database.js

--- conf/database.js
+++ conf/database.js
@@ -1,9 +1,9 @@
 if (process.env.NODE_ENV === 'production'){
     module.exports = {
-        mongoURI : "mongodb://devryAdmin:d3vryPassword@ds229474.mlab.com:29474/devry_prod"
+        mongoURI : ""
     }
 } else {
     module.exports = {
-        mongoURI : "mongodb://localhost:27017/Devry"
+        mongoURI : ""
     }
 }
